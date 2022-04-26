c97ffdde675900a6569bb3cb0496eee867caa43b
Elodie <43825246+elodiehg@users.noreply.github.com>
2022-04-26 14:52:35 +0400

    Add files via upload

---
+++ dist/topbar.bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!******************************************!*\
+  !*** ./src/assets/javascripts/topbar.js ***!
+  \******************************************/
+console.log("topbar");
+/******/ })()
+;
+//# sourceMappingURL=topbar.bundle.js.map
